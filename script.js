87b7294d4bc388d8791032bc0e2f932724f81cb6
Programming Hero <faisaleon1910@gmail.com>
2025-08-29 10:02:47 +0600

    added more function

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 // heart increase
 let firstHeart = 0;
-document.getElementById("btn-heart").addEventListener("click", function () {
+document.getElementById("btn-heart1").addEventListener("click", function () {
+  firstHeart++;
+  document.getElementById("heart-number").innerText = firstHeart;
+});
+//
+document.getElementById("btn-heart2").addEventListener("click", function () {
+  firstHeart++;
+  document.getElementById("heart-number").innerText = firstHeart;
+});
+document.getElementById("btn-heart3").addEventListener("click", function () {
+  firstHeart++;
+  document.getElementById("heart-number").innerText = firstHeart;
+});
+document.getElementById("btn-heart4").addEventListener("click", function () {
+  firstHeart++;
+  document.getElementById("heart-number").innerText = firstHeart;
+});
+document.getElementById("btn-heart5").addEventListener("click", function () {
+  firstHeart++;
+  document.getElementById("heart-number").innerText = firstHeart;
+});
+document.getElementById("btn-heart6").addEventListener("click", function () {
+  firstHeart++;
+  document.getElementById("heart-number").innerText = firstHeart;
+});
+document.getElementById("btn-heart7").addEventListener("click", function () {
+  firstHeart++;
+  document.getElementById("heart-number").innerText = firstHeart;
+});
+document.getElementById("btn-heart8").addEventListener("click", function () {
+  firstHeart++;
+  document.getElementById("heart-number").innerText = firstHeart;
+});
+document.getElementById("btn-heart9").addEventListener("click", function () {
   firstHeart++;
   document.getElementById("heart-number").innerText = firstHeart;
 });
@@ -8,14 +41,119 @@ document.getElementById("btn-heart").addEventListener("click", function () {
 // call click
 let firstCoin = 100;
 document.getElementById("call-btn").addEventListener("click", function () {
-  const name = document.getElementById("call-name").innerText;
-  const number = document.getElementById("call-number").innerText;
+  const name = document.getElementById("call-name1").innerText;
+  const number = document.getElementById("call-number1").innerText;
   alert("Calling" + " " + name + " " + number);
 
   if (firstCoin > 0) {
     firstCoin = firstCoin - 20;
   } else {
-    alert("guma akhn");
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
+  }
+
+  document.getElementById("coin-count").innerText = firstCoin;
+});
+
+document.getElementById("call-btn2").addEventListener("click", function () {
+  const name = document.getElementById("call-name2").innerText;
+  const number = document.getElementById("call-number2").innerText;
+  alert("Calling" + " " + name + " " + number);
+
+  if (firstCoin > 0) {
+    firstCoin = firstCoin - 20;
+  } else {
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
+  }
+
+  document.getElementById("coin-count").innerText = firstCoin;
+});
+document.getElementById("call-btn3").addEventListener("click", function () {
+  const name = document.getElementById("call-name3").innerText;
+  const number = document.getElementById("call-number3").innerText;
+  alert("Calling" + " " + name + " " + number);
+
+  if (firstCoin > 0) {
+    firstCoin = firstCoin - 20;
+  } else {
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
+  }
+
+  document.getElementById("coin-count").innerText = firstCoin;
+});
+document.getElementById("call-btn4").addEventListener("click", function () {
+  const name = document.getElementById("call-name4").innerText;
+  const number = document.getElementById("call-number4").innerText;
+  alert("Calling" + " " + name + " " + number);
+
+  if (firstCoin > 0) {
+    firstCoin = firstCoin - 20;
+  } else {
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
+  }
+
+  document.getElementById("coin-count").innerText = firstCoin;
+});
+document.getElementById("call-btn5").addEventListener("click", function () {
+  const name = document.getElementById("call-name5").innerText;
+  const number = document.getElementById("call-number5").innerText;
+  alert("Calling" + " " + name + " " + number);
+
+  if (firstCoin > 0) {
+    firstCoin = firstCoin - 20;
+  } else {
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
+  }
+
+  document.getElementById("coin-count").innerText = firstCoin;
+});
+document.getElementById("call-btn6").addEventListener("click", function () {
+  const name = document.getElementById("call-name6").innerText;
+  const number = document.getElementById("call-number6").innerText;
+  alert("Calling" + " " + name + " " + number);
+
+  if (firstCoin > 0) {
+    firstCoin = firstCoin - 20;
+  } else {
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
+  }
+
+  document.getElementById("coin-count").innerText = firstCoin;
+});
+document.getElementById("call-btn7").addEventListener("click", function () {
+  const name = document.getElementById("call-name7").innerText;
+  const number = document.getElementById("call-number7").innerText;
+  alert("Calling" + " " + name + " " + number);
+
+  if (firstCoin > 0) {
+    firstCoin = firstCoin - 20;
+  } else {
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
+  }
+
+  document.getElementById("coin-count").innerText = firstCoin;
+});
+document.getElementById("call-btn8").addEventListener("click", function () {
+  const name = document.getElementById("call-name8").innerText;
+  const number = document.getElementById("call-number8").innerText;
+  alert("Calling" + " " + name + " " + number);
+
+  if (firstCoin > 0) {
+    firstCoin = firstCoin - 20;
+  } else {
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
+  }
+
+  document.getElementById("coin-count").innerText = firstCoin;
+});
+document.getElementById("call-btn9").addEventListener("click", function () {
+  const name = document.getElementById("call-name9").innerText;
+  const number = document.getElementById("call-number9").innerText;
+  alert("Calling" + " " + name + " " + number);
+
+  if (firstCoin > 0) {
+    firstCoin = firstCoin - 20;
+  } else {
+    alert("You don't have enough coins! Calling requires at least 20 coins.");
   }
 
   document.getElementById("coin-count").innerText = firstCoin;
